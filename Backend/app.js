2f0a4cda98f725038f6c67293f125a198a684459
Your <yrukshan345@gmail.com>
2026-02-22 19:23:16 +0530

    Add Async/Await example

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+const condition = true; // or false to test rejection
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) resolve('Success!');
+  else reject('Failure!');
+});
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
